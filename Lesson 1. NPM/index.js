6506da966076774a016eb81fa909d20d6c2e90db
dev <dev@dev.ru>
2018-03-15 10:06:48 +0300

    add reverse message logic

--- Lesson 1. NPM/index.js
+++ Lesson 1. NPM/index.js
@@ -2,5 +2,5 @@ const Credentials = require('./credentials');
 const TeleBot = require('telebot');
 const bot = new TeleBot(Credentials.getTelegramBotToken());
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+bot.on('text', (msg) => msg.reply.text(msg.text.split("").reverse().join("")));
 bot.start();
