d6ab669390f77efeb841336d138a9f6202b37386
Roman Slobodenyuk <roman.d.slobodenyuk@ya.ru>
2018-03-22 17:48:10 +0300

    Encoding problem

--- Lesson 3. Network/index.js
+++ Lesson 3. Network/index.js
@@ -1,15 +1,23 @@
-var request = require('request');
-var cheerio = require('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
 
-request('http://bash.im/abysstop', function (error, response, html) {
-  console.log("error:\n", error, "\n----");
-  if (!error && response.statusCode == 200) {
-    console.log("html\n", html, "\n----");
-    var $ = cheerio.load(html);
-    $('#body'/* > .quote > div.text'*/).each(function(i, element){
-      console.log(element);
-      //var cols = $(this).find('td');
-      //console.dir(`${cols.eq(0).text()} ${cols.eq(1).text()} ${cols.eq(2).text()}`);
-    });
-  }
+request({
+    url: 'http://bash.im/abysstop',
+    encoding: null
+  }, 
+  function (error, response, html) {
+    console.log("error:\n", error, "\n----");
+    if (!error && response.statusCode == 200) {
+      console.log("headers\n", response.headers, "\n----");
+      //console.log("html\n", html, "\n----");
+      var $ = cheerio.load(html);
+      $('#body div.quote div.text').each(function(i, element){
+        let text = $(element).text();
+        text = iconv.decode(new Buffer(text), "utf-8").toString()
+        console.log(text);
+        //var cols = $(this).find('td');
+        //console.dir(`${cols.eq(0).text()} ${cols.eq(1).text()} ${cols.eq(2).text()}`);
+      });
+    }
 });
