6092be9766f99a4ff9d37bb2ced31c5d2350b0de
dev <dev@dev.ru>
2018-04-04 15:19:34 +0300

    Not authenticated mongo connection

--- Lesson 5. Db/server.js
+++ Lesson 5. Db/server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const Credentials = require('./credentials');
 
 const port = 8000;
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const run = () => {
-  const db = await MongoClient.connect(Credentials.getMongoDbConnectionString()); 
+const run = async () => {
+  const mongoConnection = Credentials.getMongoDbConnection();
+  const mongoClient = await MongoClient.connect(mongoConnection.host, mongoConnection.auth); 
   app.listen(port, () => {
-    console.log('We are live on ' + port);
-    db.close();
+    console.log(`Started on ${port}`);
+    const db = mongoClient.db('njslessons');
+
+    const test = { f1: 'hello', f2: 'world'};
+    db.collection('test').insert(test, (err, results) => {
+      console.log('inserted');
+      console.log('err', err);
+      console.log('results ', results);
+      const res = db.collection('test').find().toArray().then(res => console.log('res: ', res), err => console.log('err: ', res));
+      
+
+      mongoClient.close();
+    });
   });
 };
+
+run();
